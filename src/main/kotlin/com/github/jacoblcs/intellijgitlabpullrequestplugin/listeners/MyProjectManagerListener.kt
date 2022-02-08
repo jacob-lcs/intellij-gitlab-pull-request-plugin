c018d5b185f8fd91f152cb1c9c519aef84cb8ed7
GitHub Action <action@github.com>
2022-02-08 10:06:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/jacoblcs/intellijgitlabpullrequestplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/jacoblcs/intellijgitlabpullrequestplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.jacoblcs.intellijgitlabpullrequestplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.jacoblcs.intellijgitlabpullrequestplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/jacoblcs/intellijgitlabpullrequestplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jacoblcs.intellijgitlabpullrequestplugin.services
+
+import com.github.jacoblcs.intellijgitlabpullrequestplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
